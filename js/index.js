8cbb3d63a556fde9e90487dc63d1eeadbc048e4f
ishimwezachee <zacheeishimwe@gmail.com>
2022-05-19 17:10:31 +0200

    Add the cross button

---
+++ js/index.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('#menu_btn');
+    console.log(btn);
+    const menu = document.querySelector('#mobile-nav');
+    console.log(menu);
+    const close = document.querySelector('#mobile-nav span');
+    console.log(close);
+    const list = document.querySelectorAll('#mobile-nav ul li');
+    // console.log(list);
+ 
